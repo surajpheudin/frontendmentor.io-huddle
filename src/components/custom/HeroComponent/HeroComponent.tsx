e4b3b26d94fcf16669258b318997b48e10d46808
Suraj Pheudin <pheudinsuraj@gmail.com>
2022-12-06 17:03:27 +0545

    feat: add readme file

--- src/components/custom/HeroComponent/HeroComponent.tsx
+++ src/components/custom/HeroComponent/HeroComponent.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const HeroComponent: FC = () => {
   return (
-    <div className="bg-veryPaleCyan min-h-[100vh] bg-[url('/images/bg-hero-desktop.svg')] grid content-center pb-[100px] md:pb-0">
+    <div className="bg-veryPaleCyan min-h-[100vh] bg-[url('/images/bg-hero-mobile.svg')] md:bg-[url('/images/bg-hero-desktop.svg')] grid content-center pb-[100px] md:pb-0">
       <div className="container px-8 grid gap-8 md:gap-4 grid-cols-1 md:grid-cols-2 ">
         <div className="grid gap-8 h-fit max-w-[500px] p-4 mt-[100px] md:mt-0">
           <h1 className="text-2xl md:text-4xl text-center md:text-left">
